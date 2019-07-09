a4367ab9d30838235b320bd1b907e6bfa7826ca2
thiepwong <thiep.wong@gmail.com>
2019-07-09 18:03:23 +0700

    Cap nhat

--- repositories/resident.repository.go
+++ repositories/resident.repository.go
@@ -1,6 +1,8 @@
 package repositories
 
 import (
+	"errors"
+
 	"github.com/go-pg/pg"
 	"github.com/thiepwong/resident-manager/models"
 )
@@ -8,6 +10,9 @@ import (
 type ResidentRepository interface {
 	GetPagination(string, string, int, int, string) (*[]models.ResidentRoom, error)
 	GetById(string) (*models.ResidentRoom, error)
+	Add(*models.Resident, *models.ResidentRoom) (*models.ResidentRoom, error)
+	Update(*models.Resident) (*models.ResidentRoom, error)
+	Delete(*models.Resident, *models.ResidentRoom) (bool, error)
 }
 
 type residentRepositoryContext struct {
@@ -44,4 +49,19 @@ func (r *residentRepositoryContext) GetById(id string) (*models.ResidentRoom, er
 	var _resident models.ResidentRoom
 	r.db.Model(&_resident).Column("resident_room_mapping.*", "Resident", "Room", "Room.Block.Side", "Room.Side", "Room.Block").Where("resident_room_mapping.resident_id=?", id).Select()
 	return &_resident, nil
+}
+
+func (r *residentRepositoryContext) Add(res *models.Resident, roo *models.ResidentRoom) (*models.ResidentRoom, error) {
+
+	e := r.db.Insert(res)
+	if e != nil {
+		return nil, errors.New("Insert error!")
+	}
+	roo.ResidentId = res.Id
+	e = r.db.Insert(roo)
+	return roo, e
+}
+
+func (r *residentRepositoryContext) Update(res *models.Resident) (*models.ResidentRoom, error) {
+
 }
